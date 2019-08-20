b4a72615073c4814fbbd86ec56bd1fcc0db1dc05
Alejandro Endo <alejoendo@hotmail.com>
2019-08-20 14:01:26 -0500

    UI

---
+++ firebase_bootcamp/lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Dashboard'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('You are logged in'),
+              SizedBox(height: 15.0),
+              new OutlineButton(
+                borderSide: BorderSide(
+                    color: Colors.red, style: BorderStyle.solid, width: 3.0),
+                child: Text('Logout'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
