4ba82fdc1a854fc6b0864edcf1a859ba3adedd48
Alejandro Endo <alejoendo@hotmail.com>
2019-08-26 15:57:41 -0500

    .

---
+++ whatsupcoders/lib/strings.dart
@@ -0,0 +1,10 @@
+class Strings {
+  static final appBarTittle = 'Title Screen';
+  static final appHeadingTitle = 'This is my heading';
+  static final appSubHeadingTitle = 'This is my sub Heading';
+  static final appButtonCall = 'Call';
+  static final appButtonAirplane = 'Airplane';
+  static final appButtonChat = 'Chat';
+  static final appButtonMusic = 'Music';
+
+}
